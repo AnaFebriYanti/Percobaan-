26128a4622830a28b39c661f22d387934df539b7
AnaFebriYanti <80628604+AnaFebriYanti@users.noreply.github.com>
2021-06-05 14:52:03 +0700

    Update selection sort.php

--- selection sort.php
+++ selection sort.php
@@ -1,23 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-echo "Selection Sort";
-echo "<br>";
-$data=array(7,9,8,5,4,1,2,3);
+echo "//selection sort<br>";
+$data=array(6,5,3,1,8,7,2,4);
 function selection_sort($data){
-  $n=count($data);
-  for ($i = 0;$i<$n;$i++){
-    $k = $i;
-    for ($j = $i+1;$j<$n;$j++){
-      if ($data[$j] < $data[$k]) $k = $j;
-    }   
-    $dummy=$data[$i];
-    $data[$i]=$data[$k];
-    $data[$k]=$dummy;
-  }
-  return $data;
-}
-echo "Random Data = ";
-echo implode ($data);
+print_r($data); 
 echo "<br>";
-echo "Sequence Data = ";
-echo implode (selection_sort($data));
+  $n=count($data);
+  echo "jumlah data :".$n."<br>";
+
+  for ($i = 0;$i<$n;$i++){
+      $k = $i; 
+    echo "<br>";
+    echo "perulangan : ".$i."<br>";  
+    for ($j = $i+1;$j<$n;$j++){
+          if ($data[$j] < $data[$k]) $k = $j;
+    } 
+              echo "data ke: ".$i. " : ".$data[$i]." < "."data ke: ".($i+1). " : ".$data[$i+1]." = tukar";
+              $dummy=$data[$i];
+              $data[$i]=$data[$k];
+              $data[$k]=$dummy;
+              echo "<br>";          
+            }
+            return $data;
+      } 
+      
+      print_r($data);
+      echo implode (selection_sort($data));
 ?>
+
+</body>
+</html>
